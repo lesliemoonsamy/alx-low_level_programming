82b1b7b544dce63f86f44c88a1fdccca1afa58fe
lesliemoonsamy <lesliemoonsamy@outlook.com>
2023-03-18 12:20:41 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stido.h>
 /**
- * main - A program that prints a line using printf function
+ * main - Entry poin
  * Return: - set to 0 program runs successfully
  */
 int main(void)
 {
-	printf("\"with proper grammar,but the outcome is a piece of art,\n")
+	printf("with proper grammar,but the outcome is a piece of art,\n");
 		return (0);
 }
