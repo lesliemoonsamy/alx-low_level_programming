587c92bc4dfac7f44a46352a5bba55be91472669
lesliemoonsamy <lesliemoonsamy@outlook.com>
2023-03-18 11:53:12 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stido.h>
+/**
+ * main - A program that prints a line using printf function
+ * Return: - set to 0 program runs successfully
+ */
+int main(void)
+{
+	printf("\"with proper grammar,but the outcome is a piece of art,\n")
+		return (0);
+}
