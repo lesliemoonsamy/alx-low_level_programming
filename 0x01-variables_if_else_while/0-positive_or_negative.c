06688812113f09bea5662134c8012da1b2d0e6e6
lesliemoonsamy <lesliemoonsamy@outlook.com>
2023-03-20 13:06:35 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,8 +13,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
-	{
+	if (n > 0)
+{
 		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
@@ -26,4 +26,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
-}	
+}
