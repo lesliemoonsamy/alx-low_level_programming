3187175c5bf5055160ac697ef7860936b0fd747c
lesliemoonsamy <lesliemoonsamy@outlook.com>
2023-03-28 14:40:04 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,10 +5,10 @@
 #include <unistd.h>
 
 int _putchar(char c);
-void reset_to+98(int *n);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void _puts(chat *str);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
