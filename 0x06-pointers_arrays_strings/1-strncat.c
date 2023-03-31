b4da0b25421e753bab044ccdd89cd85d1f9f0a85
lesliemoonsamy <lesliemoonsamy@outlook.com>
2023-03-31 06:45:32 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,6 +11,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
+
 	i = 0;
 	while (dest[i] != '\0')
 	{
@@ -19,9 +20,9 @@ char *_strncat(char *dest, char *src, int n)
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-	dest[i] = src[j];
-	i++;
-	j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,6 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0 ; i < n ; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
+		dest[i] = src[i];	
 	return (dest);
 }
