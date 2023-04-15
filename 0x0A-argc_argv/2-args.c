6fbe6e170c3c8d3e05faf4d4d5f845f3e6c5296d
lesliemoonsamy <lesliemoonsamy@outlook.com>
2023-04-15 11:30:12 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,17 +5,15 @@
  * main - prints all arguments it receives
  * @argc: number of arguments
  * @agrv: array of arguments
- *
- * Return: Always 0 (Success)
+ * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-	{
 		printf("%s\n", argv[i]);
-	}
 
 	return (0);
 }
