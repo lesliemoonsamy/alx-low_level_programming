8af00e80d10f815d0db5df834c7c888868b18f17
Leslie Moonsamy <lesliemoonsamy@outlook.com>
2023-05-01 06:56:56 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
- * @next: points to the next node 
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
