27e1c335176342a912fd79fb7f1e4f411b75b02d
Leslie Moonsamy <lesliemoonsamy@outlook.com>
2023-05-05 14:16:20 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,14 +13,14 @@ void print_binary(unsigned long int n)
 	{
 		current = n >> i;
 
-	if (current & 1)
-	{
-		_putchar('1');
-		count++;
-	}
-	else if (count)
-		_putchar ('0');
-	}
-	if (!count)
-	_putchar('0');
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar ('0');
+		}
+		if (!count)
+			_putchar('0');
 }
