482d3331e019cb7252c713ae63b0221f91f90efe
Leslie Moonsamy <lesliemoonsamy@outlook.com>
2023-06-22 08:52:33 -0700

    Update

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,10 @@
 #ifndef CALC_H
 #define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct op - a struct op.
  * @op: the operator.
@@ -16,6 +21,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_funct(char *s))(int, int);
+int (*get_op_func(char *s))(int, int);
 
 #endif
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  *
  * Return: always 0.
  */
-int main(int _attribute_((_unused_)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
